c5dba498e34b2af8da93fdba2f2202df2e8cf169
HitakshiAjmera <64796906+HitakshiAjmera@users.noreply.github.com>
2026-06-30 10:07:33 +0530

    configuring Kafka with SpringBoot
    Project is related to learning of Kafka

---
+++ user-service/src/test/java/com/hitakshi/kafkaProject/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitakshi.kafkaProject.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
